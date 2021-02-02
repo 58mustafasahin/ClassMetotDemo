45583d21f4f9be72d9f9e80335fd3dcdef31d4d9
58mustafasahin <75518877+58mustafasahin@users.noreply.github.com>
2021-02-02 16:21:06 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+
+
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " aramıza hoşgeldiniz :))");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " adlı müşteri silindi.");
+        }
+        public void List(Customer[] customers)
+        {
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("***********************************");
+                Console.WriteLine(customer.Id);
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Surname);
+                Console.WriteLine("***********************************");
+
+
+            }
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Mustafa";
+            customer1.Surname = "ŞAHİN";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Ali";
+            customer2.Surname = "YILMAZ";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.List(customers);
+            customerManager.Delete(customer2);
+        }
+    }
+}
